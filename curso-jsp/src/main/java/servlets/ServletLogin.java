8a49a89adb36644d5621919594c61d204868e890
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-08-06 15:11:41 -0300

    Ajuste na anotação do ServletLogin

--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -8,8 +8,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-//@WebServlet("/ServletLogin") //Mapeamento de URL que vem da tela
+//
+@WebServlet("/ServletLogin") //Mapeamento de URL que vem da tela
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
